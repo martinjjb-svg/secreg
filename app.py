6d6ebc1b07f6438d4ae31c7c30b5d55654578de3
Martin <martinjjb@gmail.com>
2022-01-29 16:01:11 +0000

    add routes for questionA and question1, form.forms.py and questions and sec_definition html forms

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
-from flask import Flask, render_template, url_for
-
+from flask import Flask, render_template, url_for, redirect
+from forms.forms import Questions, QuestionA
 
 app = Flask(__name__)
+
 
 
 @app.route("/")
@@ -12,6 +13,45 @@ def index():
 @app.route("/about")
 def about():
     return render_template("about.html")
+
+
+@app.route("/questionA", methods=["GET", "POST"])
+def questionA():
+    form = QuestionA()
+
+    if form.is_submitted():
+        answer1 = form.question1.data
+        answer2 = form.question2.data
+        answer3 = form.question3.data
+        answer4 = form.question4.data
+        answer5 = form.question5.data
+        if answer1 == 'yes' or answer2 == 'yes' or answer3 == 'yes' or answer4 == 'yes':
+            if answer5 == 'no':
+                return redirect(url_for("question1"))
+            else:
+                return render_template('index.html')
+        else:
+            return render_template('index.html')
+
+    else:
+        return render_template('sec_definition.html', form=form)
+
+
+@app.route("/question1", methods=["GET", "POST"])
+def question1():
+    form = Questions()
+
+    if form.is_submitted():
+        answer = form.question2.data
+        if answer == "a":
+            return redirect(url_for('index'))
+        elif answer == "b":
+            return redirect(url_for('question1'))
+
+        else:
+            return render_template('questions.html', form=form)
+
+    return render_template('questions.html', form=form)
 
 
 if __name__ == '__main__':
---
+++ forms/forms.py
@@ -0,0 +1,29 @@
+from flask_wtf import FlaskForm
+from wtforms import RadioField, SubmitField, SelectField
+from wtforms.validators import DataRequired, Length, EqualTo
+
+
+class Questions(FlaskForm):
+    question2 = SelectField("level_1_question_1",
+                            choices=[('a', 'a'), ('b', 'b'), ('c', 'c')], validators=[DataRequired()])
+    submit = SubmitField('submit')
+
+
+class QuestionA(FlaskForm):
+    question1 = RadioField("""level_4_question_1""",
+                           choices=[('yes', 'yes'), ('no', 'no')],
+                           validators=[DataRequired()])
+    question2 = RadioField("""level_4_question_2""",
+                           choices=[('yes', 'yes'), ('no', 'no')],
+                           validators=[DataRequired()])
+    question3 = RadioField("""level_4_question_3""",
+                           choices=[('yes', 'yes'), ('no', 'no')],
+                           validators=[DataRequired()])
+    question4 = RadioField("""level_4_question_4""",
+                           choices=[('yes', 'yes'), ('no', 'no')],
+                           validators=[DataRequired()])
+    question5 = RadioField("""level_4_question_5""",
+                           choices=[('yes', 'yes'), ('no', 'no')],
+                           validators=[DataRequired()])
+
+    submit = SubmitField('submit')
