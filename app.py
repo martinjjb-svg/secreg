6269acf006c5242b6c562c598256349f24d46523
Martin <martinjjb@gmail.com>
2022-02-04 22:21:53 +0000

    Revert to earlier version

--- app.py
+++ app.py
@@ -1,22 +1,13 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, url_for
 
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '453672hhy678'
 
 
-@app.route("/form")
+@app.route("/")
 def index():
-    return render_template("form.html")
-
-
-@app.route('/data/', methods = ['POST', 'GET'])
-def data():
-    if request.method == 'GET':
-        return f"The URL /data is accessed directly. Try going to '/form' to submit form"
-    if request.method == 'POST':
-        form_data = request.form
-        return render_template('data.html',form_data = form_data)
+    return render_template("index.html")
 
 
 @app.route("/about")
