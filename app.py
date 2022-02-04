3f76f182b0d9ce236cc9d57e3794469e9544647e
Martin <martinjjb@gmail.com>
2022-02-04 22:50:55 +0000

    Adding questionA

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, url_for, redirect
-from form.forms import Questions
+from form.forms import Questions, QuestionA
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '453672hhy678'
@@ -13,6 +13,28 @@ def index():
 @app.route("/about")
 def about():
     return render_template("about.html")
+
+
+@app.route("/questionA", methods=["GET", "POST"])
+def questionA():
+    form = QuestionA()
+
+    if form.is_submitted():
+        answer1 = form.question1.data
+        answer2 = form.question2.data
+        answer3 = form.question3.data
+        answer4 = form.question4.data
+        answer5 = form.question5.data
+        if answer1 == 'yes' or answer2 == 'yes' or answer3 == 'yes' or answer4 == 'yes':
+            if answer5 == 'no':
+                return redirect(url_for("question1"))
+            else:
+                return render_template('index.html')
+        else:
+            return render_template('index.html')
+
+    else:
+        return render_template('sec_definition.html', form=form)
 
 
 @app.route("/question1", methods=["GET", "POST"])
--- form/forms.py
+++ form/forms.py
@@ -7,3 +7,22 @@ class Questions(FlaskForm):
     question2 = SelectField("level_1_question_1",
                             choices=[('a', 'a'), ('b', 'b'), ('c', 'c')], validators=[DataRequired()])
     submit = SubmitField('submit')
+
+
+class QuestionA(FlaskForm):
+    question1 = RadioField("""level_4_question_1""",
+                           choices=[('yes', 'yes'), ('no', 'no')],
+                           validators=[DataRequired()])
+    question2 = RadioField("""level_4_question_2""",
+                           choices=[('yes', 'yes'), ('no', 'no')],
+                           validators=[DataRequired()])
+    question3 = RadioField("""level_4_question_3""",
+                           choices=[('yes', 'yes'), ('no', 'no')],
+                           validators=[DataRequired()])
+    question4 = RadioField("""level_4_question_4""",
+                           choices=[('yes', 'yes'), ('no', 'no')],
+                           validators=[DataRequired()])
+    question5 = RadioField("""level_4_question_5""",
+                           choices=[('yes', 'yes'), ('no', 'no')],
+                           validators=[DataRequired()])
+    submit = SubmitField('submit')
