70a22d86f5b5f85b20ebd97976ae9eeb1485193d
Martin <martinjjb@gmail.com>
2022-02-07 18:11:33 +0000

    Renaming about to definitions

--- app.py
+++ app.py
@@ -10,9 +10,9 @@ def index():
     return render_template("index.html")
 
 
-@app.route("/about")
-def about():
-    return render_template("about.html")
+@app.route("/definitions")
+def definitions():
+    return render_template("definitions.html")
 
 
 @app.route("/questionA", methods=["GET", "POST"])
